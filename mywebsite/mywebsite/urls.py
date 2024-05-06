46c87bae66b945a081dfa798e0ac16eff271d6c8
= <=>
2024-05-07 03:23:05 +0530

    Adding the code for the statcic image path

--- mywebsite/mywebsite/urls.py
+++ mywebsite/mywebsite/urls.py
@@ -16,12 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('blog_app.urls'))  #leaving the route empty so that is will go to hme of the blog_app
 
-]  
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
    
    #including the path from blog_app in project, here the include 
      #is chopping off the blog_app from .urls because first it goes to blog_app already 
