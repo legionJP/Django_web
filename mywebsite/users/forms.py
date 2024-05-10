57d429df82e24a88afbbc4b1b8afcade3d0a8bc4
= <=>
2024-05-10 12:12:11 +0530

    implement  user profile updates

--- mywebsite/users/forms.py
+++ mywebsite/users/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User 
+from .models import Profile
 from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm): #for adding the email in usercreation form
@@ -8,3 +9,15 @@ class UserRegisterForm(UserCreationForm): #for adding the email in usercreation 
     class Meta:    #Thsis class gives us the nested namspace for the configurtion and keeps the configuration in one place
         model = User
         fields = ['username','email','password1','password2']
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    class Meta: 
+        model = User
+        fields = ['username','email'] 
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile     
+        fields =['image']
+                  
--- mywebsite/users/views.py
+++ mywebsite/users/views.py
@@ -2,27 +2,45 @@ from django.shortcuts import render , redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 #from django.contrib.auth.forms import UserCreationForm #for creating the from from django 
-from .forms import UserRegisterForm #using it in the place of  Usercreation 
+from .forms import UserRegisterForm,UserUpdateForm , ProfileUpdateForm #using it in the place of  Usercreation 
 
 
 # Create your views here.
 
 def register(request):
     if request.method =='POST':     
-        form =UserRegisterForm(request.POST) #instantiating the form if we get any post request of user form 
+        form =UserRegisterForm(request.POST)                     #instantiating the form if we get any post request of user form 
         if form.is_valid():
-            form.save()  #it will save and hashed the password 
-            username = form.cleaned_data.get('username') #cleaned_data is dict where the data will be stored 
+            form.save()                                            #it will save and hashed the password 
+            username = form.cleaned_data.get('username')           #cleaned_data is dict where the data will be stored 
             messages.success(request,f'Account created for {username}! You can Login Now.')
-            return redirect('login') #redirecting to the  home by name of the blog_app home 
+            return redirect('login')                                           #redirecting by blog_app home 
     else:
         form = UserRegisterForm()    
     return render(request, 'users/register.html', {'form': form}) #passing the form as a context so we can access form within the template
 
+
 @login_required
 def profile(request):
-    return render(request , 'users/profile.html')
+    if request.method =='POST':            
+        u_form = UserUpdateForm(request.POST, instance=request.user)                  #instanciating the form in login required
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
 
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your Account Profile has been updated')
+            return redirect('profile')  #for avoiding the get request from the browser
+    else:
+        u_form = UserUpdateForm(instance=request.user)                   
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+        
+    #passing the  form to template by context
+    context ={
+            'u_form': u_form,
+            'p_form': p_form
+    }
+    return render(request , 'users/profile.html',context)
 
 # message.debug
 # message.info , messages.success, message.warning, message.error
